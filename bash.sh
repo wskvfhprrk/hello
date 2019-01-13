91a6f3a3d46ed637ffe79443a7154a2484b0cab7
hejz <75412985@qq.com>
2019-01-13 16:39:32 +0800

    123

--- bash.sh
+++ bash.sh
@@ -7,8 +7,7 @@ git pull
 mvn clean package -Dmaven.test.skip=true
 
 #docker
-docker build -t hello .
-docker rm -f hello
-docker run -p 8001:8080 --name hello hello
+docker build -t hello . && docker rm -f hello
+docker run -d -p 8080:8080 --name hello hello
 
 
